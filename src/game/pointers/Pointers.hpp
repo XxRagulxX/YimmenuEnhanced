f516ce3e340543d2aecdd8a5c4d4c3e1356ee709
Arthur <121949966+ShinyWasabi@users.noreply.github.com>
2025-04-13 20:44:21 +0300

    feat: Format Money (#180)
    * feat: Format Money
    
    * Code review changes

--- src/game/pointers/Pointers.hpp
+++ src/game/pointers/Pointers.hpp
@@ -132,6 +132,8 @@ namespace YimMenu
 		Functions::GetPresenceAttributes GetPresenceAttributes;
 		Functions::GetAvatars GetAvatars;
 		std::uint32_t* NetworkTime;
+		PVOID FormatIntCaller1;
+		PVOID FormatIntCaller2;
 	};
 
 	struct Pointers : PointerData
