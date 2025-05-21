45df674b8c6b9c9841ff15c69cb43d24cc617c46
XxRagulxX <donragulvijay@gmail.com>
2025-05-21 07:42:15 +0530

    Recovery: Updated

--- src/game/frontend/submenus/Recovery.cpp
+++ src/game/frontend/submenus/Recovery.cpp
@@ -1,6 +1,5 @@
 #include "Recovery.hpp"
 #include "game/frontend/items/Items.hpp"
-#include "game/features/recovery/GiveVehicleReward.hpp"
 #include "Recovery/StatEditor.hpp"
 #include "Recovery/Transactions.hpp"
 #include "Recovery/HeistModifier.hpp"
@@ -10,9 +9,7 @@ namespace YimMenu::Submenus
 	Recovery::Recovery() :
 	    Submenu::Submenu("Recovery")
 	{
-		auto shopping = std::make_shared<Category>("Shopping");
 		auto missions = std::make_shared<Category>("Missions");
-		auto vehiclesGroup = std::make_shared<Group>("Vehicles");
 		auto generalGroup = std::make_shared<Group>("General");
 		auto unlocker = std::make_shared<Category>("Unlocks");
 		auto bunker = std::make_shared<Group>("Bunker Research");
@@ -23,18 +20,6 @@ namespace YimMenu::Submenus
 		auto gtaplus  = std::make_shared<Group>("GTA+");
 		auto business = std::make_shared<Group>("Business");
 		auto vehicle = std::make_shared<Group>("Vehicle");
-
-		vehiclesGroup->AddItem(std::make_shared<BoolCommandItem>("dlcvehicles"_J));
-
-		vehiclesGroup->AddItem(std::make_unique<ImGuiItem>([] {
-			if (ImGui::Button("Save This Vehicle as Personal Vehicle"))
-			{
-				if (GiveVehicleReward::IsSafeToRunScript())
-				{
-					GiveVehicleReward::SetShouldRunScript(true);
-				}
-			}
-		}));
 
 		generalGroup->AddItem(std::make_shared<BoolCommandItem>("playallmissionssolo"_J));
 		generalGroup->AddItem(std::make_shared<CommandItem>("forcelaunchheist"_J));
@@ -61,7 +46,6 @@ namespace YimMenu::Submenus
 		awards->AddItem(std::make_shared<CommandItem>("unlockcollectables"_J));
 		business->AddItem(std::make_shared<CommandItem>("resupplybusiness"_J));
 		business->AddItem(std::make_shared<BoolCommandItem>("supplycooldownbypass"_J));
-		shopping->AddItem(vehiclesGroup);
 		missions->AddItem(generalGroup);
 		unlocker->AddItem(awards);
 		unlocker->AddItem(bunker);
@@ -71,7 +55,6 @@ namespace YimMenu::Submenus
 		unlocker->AddItem(gtaplus);
 		unlocker->AddItem(business);
 		unlocker->AddItem(vehicle);
-		AddCategory(std::move(shopping));
 		AddCategory(std::move(missions));
 		AddCategory(std::move(unlocker));
 		AddCategory(BuildStatEditorMenu());
