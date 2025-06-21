a184de335cdca7383b85181ce5a6f1365a6ab016
kurumimeow <kurumitokisakicat@gmail.com>
2025-06-22 05:16:28 +0530

    update some locals and fix misleading globals and locals declaration (#531)

--- src/game/features/recovery/Heist/CayoPericoHeist.cpp
+++ src/game/features/recovery/Heist/CayoPericoHeist.cpp
@@ -10,6 +10,7 @@
 #include "core/backend/ScriptMgr.hpp"
 #include "game/backend/Tunables.hpp"
 #include "game/backend/DeleteObjectsByHash.hpp"
+#include "core/backend/FiberPool.hpp"
 
 namespace YimMenu::Features
 {
@@ -28,10 +29,10 @@ namespace YimMenu::Features
 			{
 				auto base = ScriptGlobal(1975799).At(831).At(56);
 
-				*base.At(1).As<int*>() = _CayoPericoHeistCut1.GetState();
-				*base.At(2).As<int*>() = _CayoPericoHeistCut2.GetState();
-				*base.At(3).As<int*>() = _CayoPericoHeistCut3.GetState();
-				*base.At(4).As<int*>() = _CayoPericoHeistCut4.GetState();
+				*base.At(0, 1).As<int*>() = _CayoPericoHeistCut1.GetState();
+				*base.At(1, 1).As<int*>() = _CayoPericoHeistCut2.GetState();
+				*base.At(2, 1).As<int*>() = _CayoPericoHeistCut3.GetState();
+				*base.At(3, 1).As<int*>() = _CayoPericoHeistCut4.GetState();
 			}
 		};
 
@@ -43,20 +44,36 @@ namespace YimMenu::Features
 			{
 				auto base = ScriptGlobal(1976911);
 
-				*base.At(0, 27).At(8).At(0).As<int*>() = 1;
-				*base.At(1, 27).At(8).At(1).As<int*>() = 1;
-				*base.At(2, 27).At(8).At(2).As<int*>() = 1;
-				*base.At(3, 27).At(8).At(3).As<int*>() = 1;
+				for (int i = 0; i <= 3; i++)
+				{
+					*base.At(i, 27).At(7).At(i, 1).As<int*>() = 1;
+				}
 			}
 		};
 
-		static std::vector<std::pair<int, const char*>> cayoPericoHeistDifficulty = {{126823, "Normal"}, {131055, "Hard"}};
+		static std::vector<std::pair<int, const char*>> cayoPericoHeistDifficulty = {
+			{126823, "Normal"},
+			{131055, "Hard"}
+		};
 		static ListCommand _CayoPericoHeistDifficulty{"cayopericoheistdifficulty", "Difficulty", "Heist difficulty", cayoPericoHeistDifficulty, 126823};
 
-		static std::vector<std::pair<int, const char*>> cayoPericoHeistPrimaryTarget = {{5, "Panther Statue"}, {3, "Pink Diamond"}, {4, "Madrazo Files"}, {2, "Bearer Bonds"}, {1, "Ruby Necklace"}, {0, "Sinsimito Tequila"}};
+		static std::vector<std::pair<int, const char*>> cayoPericoHeistPrimaryTarget = {
+			{5, "Panther Statue"},
+			{3, "Pink Diamond"},
+			{4, "Madrazo Files"},
+			{2, "Bearer Bonds"},
+			{1, "Ruby Necklace"},
+			{0, "Sinsimito Tequila"}
+		};
 		static ListCommand _CayoPericoHeistPrimaryTarget{"cayopericoheistprimarytarget", "Primary Target", "Primary target", cayoPericoHeistPrimaryTarget, 5};
 
-		static std::vector<std::pair<int, const char*>> cayoPericoHeistWeapon = {{1, "Aggressor"}, {2, "Conspirator"}, {3, "Crack Shot"}, {4, "Saboteur"}, {5, "Marksman"}};
+		static std::vector<std::pair<int, const char*>> cayoPericoHeistWeapon = {
+			{1, "Aggressor"},
+			{2, "Conspirator"},
+			{3, "Crack Shot"},
+			{4, "Saboteur"},
+			{5, "Marksman"}
+		};
 		static ListCommand _CayoPericoHeistWeapon{"cayopericoheistweapon", "Weapon", "Weapon category", cayoPericoHeistWeapon, 1};
 
 		class Setup : public Command
@@ -107,16 +124,8 @@ namespace YimMenu::Features
 
 				ScriptMgr::Yield(500ms);
 
-				auto scriptLocal = ScriptLocal("heist_island_planning"_J, 1566).As<int*>();
-				if (scriptLocal && scriptLocal != nullptr)
-				{
-					*scriptLocal = 2;
-				}
-				else
-				{
-					// Log error or handle gracefully
-					return;
-				}
+				if (auto thread = Scripts::FindScriptThread("heist_island_planning"_J))
+					*ScriptLocal(thread, 1568).As<int*>() = 2;
 			}
 		};
 
@@ -187,8 +196,8 @@ namespace YimMenu::Features
 
 			virtual void OnCall() override
 			{
-				*ScriptLocal("fm_mission_controller_2020"_J, 58222).At(1357).At(53).As<int*>() =
-				    _CayoPericoHeistSecondaryTakeValue.GetState();
+				if (auto thread = Scripts::FindScriptThread("fm_mission_controller_2020"_J))
+					*ScriptLocal(thread, 58222).At(1357).At(53).As<int*>() = _CayoPericoHeistSecondaryTakeValue.GetState();
 			}
 		};
 		class SkipHacking : public Command
@@ -197,7 +206,8 @@ namespace YimMenu::Features
 
 			virtual void OnCall() override
 			{
-				*ScriptLocal("fm_mission_controller_2020"_J, 25460).As<int*>() = 5;
+				if (auto thread = Scripts::FindScriptThread("fm_mission_controller_2020"_J))
+					*ScriptLocal(thread, 25460).As<int*>() = 5;
 			}
 		};
 		class CutSewer : public Command
@@ -206,9 +216,8 @@ namespace YimMenu::Features
 
 			virtual void OnCall() override
 			{
-				*ScriptLocal("fm_mission_controller_2020"_J, 30285).As<int*>() = 6;
-				Hash drainagePipeHash = "prop_chem_grill_bit"_J;
-				YimMenu::DeleteObjectsByHash(drainagePipeHash);
+				if (auto thread = Scripts::FindScriptThread("fm_mission_controller_2020"_J))
+					*ScriptLocal(thread, 30285).As<int*>() = 6;
 			}
 		};
 
@@ -218,7 +227,8 @@ namespace YimMenu::Features
 
 			virtual void OnCall() override
 			{
-				*ScriptLocal("fm_mission_controller_2020"_J, 31525).At(3).As<float*>() = 100.0f;
+				if (auto thread = Scripts::FindScriptThread("fm_mission_controller_2020"_J))
+					*ScriptLocal(thread, 31525).At(3).As<float*>() = 100.0f;
 			}
 		};
 
@@ -228,34 +238,41 @@ namespace YimMenu::Features
 
 			virtual void OnCall() override
 			{
-				auto ped = Self::GetPed();
-
-				if (!ped)
-					return;
-
-				auto pos = ped.GetPosition();
-				auto heading = ped.GetHeading();
-				auto primary_target = Stats::GetInt("MPX_H4CNF_TARGET");
-
-				TASK::TASK_GO_STRAIGHT_TO_COORD(ped.GetHandle(), 5006.917, -5755.931, 15.484, 1.0, 3, 15, 5);
-
-				switch (primary_target)
+				if (auto thread = Scripts::FindScriptThread("fm_mission_controller_2020"_J))
 				{
-				case 0:
-				case 1:
-				case 3:
-				case 5:
-					*ScriptLocal("fm_mission_controller_2020"_J, 31524).As<int*>() = 5;
-					*ScriptLocal("fm_mission_controller_2020"_J, 31525).As<int*>() = 3;
-					break;
-				case 2:
-				case 4: *ScriptLocal("fm_mission_controller_2020"_J, 31499).As<int*>() = 7; break;
-				default: break;
+					auto ped = Self::GetPed();
+
+					if (!ped)
+						return;
+
+					auto pos = ped.GetPosition();
+					auto heading = ped.GetHeading();
+					auto primary_target = Stats::GetInt("MPX_H4CNF_TARGET");
+
+					FiberPool::Push([&] {
+						TASK::TASK_GO_STRAIGHT_TO_COORD(ped.GetHandle(), 5006.917, -5755.931, 15.484, 1.0, 3, 15, 5);
+
+						switch (primary_target)
+						{
+						case 0:
+						case 1:
+						case 3:
+						case 5:
+							*ScriptLocal(thread, 31524).As<int*>() = 5;
+							*ScriptLocal(thread, 31525).As<int*>() = 3;
+							break;
+						case 2:
+						case 4:
+							*ScriptLocal(thread, 31499).As<int*>() = 7;
+							break;
+						default: break;
+						}
+
+						ScriptMgr::Yield(4000ms);
+
+						TASK::TASK_GO_STRAIGHT_TO_COORD(ped.GetHandle(), pos.x, pos.y, pos.z, 1.0, 3, heading, 5);
+					});
 				}
-
-				ScriptMgr::Yield(4000ms);
-
-				TASK::TASK_GO_STRAIGHT_TO_COORD(ped.GetHandle(), pos.x, pos.y, pos.z, 1.0, 3, heading, 5);
 			}
 		};
 		class InfinitePlasmaCutterHeat : public LoopedCommand
@@ -280,11 +297,14 @@ namespace YimMenu::Features
 
 			virtual void OnCall() override
 			{
-				Scripts::ForceScriptHost(Scripts::FindScriptThread("fm_mission_controller_2020"_J));
-				ScriptMgr::Yield(500ms);
+				if (auto thread = Scripts::FindScriptThread("fm_mission_controller_2020"_J))
+				{
+					Scripts::ForceScriptHost(thread);
+					ScriptMgr::Yield(500ms);
 
-				*ScriptLocal("fm_mission_controller_2020"_J, 54763).As<int*>() = 9;
-				*ScriptLocal("fm_mission_controller_2020"_J, 54763).At(1776).At(1).As<int*>() = 50;
+					*ScriptLocal(thread, 54763).As<int*>() = 9;
+					*ScriptLocal(thread, 54763).At(1776).At(0, 1).As<int*>() = 50;
+				}
 			}
 		};
 
