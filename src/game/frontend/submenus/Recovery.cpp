4350dc15e30e0c874b29880094dd21e3c1045df4
XxRagulxX <donragulvijay@gmail.com>
2025-06-21 11:57:14 +0530

    Recovery: Fixed missing  Business

--- src/game/frontend/submenus/Recovery.cpp
+++ src/game/frontend/submenus/Recovery.cpp
@@ -36,6 +36,8 @@ namespace YimMenu::Submenus
 
 		businessGroup->AddItem(std::make_shared<ListCommandItem>("businesssafe"_J));
 		businessGroup->AddItem(std::make_shared<CommandItem>("claimsafeearnings"_J));
+		businessGroup->AddItem(std::make_shared<BoolCommandItem>("removeresupplycooldown"_J));
+		businessGroup->AddItem(std::make_shared<BoolCommandItem>("resupplybusiness"_J));
 
 		casinoSlots->AddItem(std::make_shared<BoolCommandItem>("casinomanipulaterigslotmachines"_J));
 
