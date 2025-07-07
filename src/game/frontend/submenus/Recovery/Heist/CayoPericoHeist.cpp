dfbd81e24cfb4420799776eea031ce5bf187b3b3
kurumimeow <kurumitokisakicat@gmail.com>
2025-07-08 03:50:20 +0530

    feat: add custom weapons (#388)
    * added custom weapons
    
    * remove SameLineItem
    
    * some fixes
    
    * no changes
    
    * more fixes
    
    * some changes
    
    (cherry picked from commit dd6907bf14d213d6ac000f72072b73571176446f)

--- src/game/frontend/submenus/Recovery/Heist/CayoPericoHeist.cpp
+++ src/game/frontend/submenus/Recovery/Heist/CayoPericoHeist.cpp
@@ -24,14 +24,8 @@ namespace YimMenu::Submenus
 		setups->AddItem(std::make_shared<CommandItem>("cayopericoheistsetup"_J));
 
 		loots->AddItem(std::make_shared<IntCommandItem>("cayopericoheistprimarytargetvalue"_J));
-		loots->AddItem(std::make_shared<ImGuiItem>([] {
-			ImGui::SameLine();
-		}));
-		loots->AddItem(std::make_shared<CommandItem>("cayopericoheistsetprimarytargetvalue"_J, "Set##primarytargetvalue"));
 		loots->AddItem(std::make_shared<IntCommandItem>("cayopericoheistsecondarytakevalue"_J));
-		loots->AddItem(std::make_shared<ImGuiItem>([] {
-			ImGui::SameLine();
-		}));
+		loots->AddItem(std::make_shared<CommandItem>("cayopericoheistsetprimarytargetvalue"_J, "Set##primarytargetvalue"));
 		loots->AddItem(std::make_shared<CommandItem>("cayopericoheistsetsecondarytakevalue"_J, "Set##secondarytakevalue"));
 
 		misc->AddItem(std::make_shared<CommandItem>("cayopericoheistskiphacking"_J));
