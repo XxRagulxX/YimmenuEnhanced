fffc57871a36b664a14b408bd6eedfd90b996f04
XxRagulxX <donragulvijay@gmail.com>
2026-01-23 08:44:16 +0530

    MinHook : Updated MinHook v1.3.4

--- cmake/minhook.cmake
+++ cmake/minhook.cmake
@@ -6,7 +6,7 @@ message(STATUS "Setting up ${LIB_NAME}")
 FetchContent_Declare(
     ${LIB_NAME}
     GIT_REPOSITORY https://github.com/TsudaKageyu/minhook.git
-    GIT_TAG        2b003bb063d66f016cc1aef7d63951350ce60f35
+    GIT_TAG        1e9ad1eb42db11bfcb65461f687c656612d1b555
 	GIT_PROGRESS TRUE
 )
 FetchContent_MakeAvailable(${LIB_NAME})
