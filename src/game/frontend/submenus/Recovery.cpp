e4a9d75b2f629d83fd21b3f087fd449a51e76a97
XxRagulxX <donragulvijay@gmail.com>
2026-07-13 13:57:30 +0530

    Heist: Fixed Solo Casino Heist

--- src/game/frontend/submenus/Recovery.cpp
+++ src/game/frontend/submenus/Recovery.cpp
@@ -26,6 +26,7 @@ namespace YimMenu::Submenus
 		//auto casinoRoulette = std::make_shared<Group>("Roulette");
 
 		generalGroup->AddItem(std::make_shared<BoolCommandItem>("playallmissionssolo"_J));
+		generalGroup->AddItem(std::make_shared<BoolCommandItem>("casinosolo"_J));
 		generalGroup->AddItem(std::make_shared<BoolCommandItem>("unlockgtaplus"_J));
 		generalGroup->AddItem(std::make_shared<BoolCommandItem>("overriderpmultiplier"_J));
 		generalGroup->AddItem(std::make_shared<ConditionalItem>("overriderpmultiplier"_J, std::make_shared<FloatCommandItem>("rpmultiplierinput"_J)));
